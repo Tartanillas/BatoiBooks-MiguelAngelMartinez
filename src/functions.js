309b7ea025b2bc77c91ba2c31ab96386960f570c
Miguel Angel Martinez <tartanasdev@gmail.com>
2024-09-25 16:48:22 +0200

    Funcion getBookById() realizada y funcionando.

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,10 @@
-export{}
+function getBookById(books, number) {
+    let libro = books.find(libro =>  libro.id === number);
+    if(!libro) {
+        throw "No existe ningún libro con ese ID.";
+    } else {
+        return libro;
+    }
+}
+
+export{getBookById}
