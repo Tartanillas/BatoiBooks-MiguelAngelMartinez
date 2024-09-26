1fa7d610b166c5d37108794446aa8b4a2ad78f5a
Miguel Angel Martinez <tartanasdev@gmail.com>
2024-09-26 20:35:02 +0200

    Funcionan todos los métodos menos el de incrementPriceOfBooks().

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import './style.css'
+import * as funciones from './src/functions';
+import './style.css';
+import data from './src/services/datos.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -9,4 +11,12 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-setupCounter(document.querySelector('#counter'))
+let librosUsuario4 = funciones.booksFromUser(data.books, 4);
+console.log(librosUsuario4);
+
+let librosModulo5021 = funciones.booksFromModule(data.books, "5021");
+let librosEnModulo5021YEnEstadoGood = funciones.booksWithStatus(librosModulo5021, "good");
+console.log(librosEnModulo5021YEnEstadoGood);
+
+let incremento = funciones.incrementPriceOfbooks(data.books, 10);
+console.log(incremento);
