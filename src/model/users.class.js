1c2e360e31d9ba4ba349ffc79b042d8ff0eeba74
Miguel Angel Martinez <tartanasdev@gmail.com>
2024-10-03 21:59:58 +0200

    clase users acabada.

--- src/model/users.class.js
+++ src/model/users.class.js
@@ -8,9 +8,13 @@ export default class Users {
             this.data[i] = new User(array[i].id, array[i].nick, array[i].email, array[i].password);
         }
     }
-    /*addUser(usuario) {
-        this.data.push(new User(usuario.nick, usuario.email, usuario.password));
-    }*/
+    addUser(usuario) {
+        let nextId = this.data.reduce((max, usuario) => usuario.id > max ? usuario.id : max, 0) + 1;
+        let nuevoUsuario = new User(nextId, usuario.nick, usuario.email, usuario.password);
+        this.data.push(nuevoUsuario);
+        return nuevoUsuario;
+    }
+    
 
     removeUser(id) {
         let usuario = this.data.findIndex((user) => user.id == id);
@@ -20,12 +24,44 @@ export default class Users {
         this.data.splice(usuario, 1);
     }
 
-    /*changeUser(id) {
-        if (usuario === -1) {
+    changeUser(usuario) {
+        let usuarioAModificar = this.data.findIndex(user => user.id === usuario.id);
+        if (usuarioAModificar === -1) {
             throw "No existe ningún usuario con ese ID.";
+        } else {
+            this.data.splice(usuarioAModificar, 1, usuario);
+            return usuario;
         }
     }
 
     toString() {
-    }*/
+        let cadena = "";
+        this.data.forEach(element => {
+            cadena += element.toString();
+        });
+        return cadena;
+    }
+
+    getUserById(userId) {
+        let usuario = this.data.find((usuario) => usuario.id === userId);
+        if (!usuario) {
+            throw "No existe ningún usuario con ese ID";
+        }
+        return usuario;
+    }
+
+    getUserIndexById(userId) {
+        let usuario = this.data.findIndex((usuario) => usuario.id === userId);
+        if (usuario === -1) {
+            throw "No existe ningún usuario con ese ID";
+        }
+        return usuario;
+    }
+    getUserByNickName(nick) {
+        let usuario = this.data.find((usuario) => usuario.nick === nick);
+        if (!usuario) {
+            throw "No existe ningún usuario con ese nick";
+        }
+        return usuario;
+    }
 }
